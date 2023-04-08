3d882fc49e0527e4298c5bca8fabc233943454c4
spot1999 <115357075+spot1999@users.noreply.github.com>
2023-04-08 22:29:00 +0800

    first time

---
+++ app.py
@@ -0,0 +1,137 @@
+import streamlit as st
+import requests
+import datetime
+from streamlit_lottie import st_lottie
+from PIL import Image
+
+def local_css(file_name):
+    with open(file_name) as f:
+        css = f.read()
+        st.markdown("<style>{}</style>".format(css), unsafe_allow_html=True)
+
+
+
+#st.image(img_f, width=width, height=height)
+
+
+st.set_page_config(page_title="My portfolio", page_icon=":tada:", layout="wide")
+
+
+def load_lottieur(url):
+    r=requests.get(url)
+    if r.status_code!=200:
+        return None
+    else:
+        return r.json()
+
+animation=load_lottieur('https://assets8.lottiefiles.com/packages/lf20_x1ikbkcj.json')
+#style1 filipino_style
+img_f=Image.open('images/sepehr-safari-sajiam-n8bZJpA0hig-unsplash.jpg')
+width, height = img_f.size
+new_size = (500, 300) # Set the new width and height
+resized_img = img_f.resize(new_size)
+resized_img.save('images/sepehr-safari-sajiam-n8bZJpA0hig-unsplash.jpg')
+
+img_s=Image.open('images/filipino_style.jpg')
+#,width=500, height=300
+now=datetime.datetime.now()
+hour=now.hour
+greet=None
+if hour < 12:
+    #st.write("Good morning!")
+    greet=" Good morning!"
+elif hour < 18:
+    # st.write("Good evening!")
+    greet =" Good afternoon!"
+else:
+    # st.write("Good evening!")
+    greet =" Good evening!"
+
+st.subheader('hi visitor:wave:'+greet)
+st.title('My name is Greyhack1999:')
+
+with st.container():
+    st.write('Hi im greyhack1999')
+with st.container():
+    st.write('---')
+    left_column,right_column=st.columns(2)
+    with left_column:
+        st.header('What I love to do:')
+        st.write('##')
+        st.write(
+            "Based on the things I like to do, they are very different from the courses I've taken:\n"
+            "- Play PUBG Mobile\n"
+            "- Sit and watch action movies\n"
+
+                 )
+        st.write('[Learn more>](https://www.freecodecamp.org/learn/)')
+
+with right_column:
+    st_lottie(animation,height=300,key='coding')
+
+
+with st.container():
+    st.write('---')
+    st.header('Original picture')
+    st.write('##')
+    image_column,text_column=st.columns((1,2))
+    with image_column:
+        #insert image
+        st.image(img_f)
+    with text_column:
+        st.subheader('This picture is from the pixel-bay website')
+        st.write('##')
+        st.write(
+            "This is the picture tat caught my attention to edit for clothings:\n"
+            "- It is a plain black cloths with the bacgground of cars\n"
+            "- looking at it makes have no sense\n"
+        )
+        #st.write('[Learn more>](https://www.freecodecamp.org/learn/)')
+with st.container():
+    st.write('---')
+    st.header('Edited picture')
+    st.write('##')
+    text_column,image_column=st.columns((1,2))
+    with image_column:
+        #insert image
+
+        st.image(img_s)
+
+    with text_column:
+        st.subheader('Filipino Style Cloth')
+        st.write('##')
+        st.write(
+            "Ive have no background in editing image but this is the first time ive created one :\n"
+            "- Sit and watch action movies\n"
+
+        )
+        #st.write('[Learn more>](https://www.freecodecamp.org/learn/)')
+
+
+
+
+
+#__contact__FORM
+
+with st.container():
+    st.write('___')
+    st.header('Get touch with me')
+    st.write('##')
+
+
+contact_form= """
+
+<form action="https://formsubmit.co/6650288b2352759c4dd8c1fed161b127" method="POST">
+     <input type='hidden' name='_captcha' value='false'>
+     <input type="text" name="name" placeholder='Your name'required>
+     <input type="email" name="email" placeholder='Your email' required>
+     <textarea name='message' placeholder='Your messafe here' required></textarea>
+     <button type="submit">Send</button>
+</form>
+
+"""
+left_column,right_column=st.columns(2)
+with left_column:
+    st.markdown(contact_form,unsafe_allow_html=True)
+with right_column:
+    st.empty()
